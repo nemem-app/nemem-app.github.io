ae2e33ce4e3da3170a7e4ee14a38f864413fef29
sufimtsev <sufimtsev@ozon.ru>
2022-07-24 20:48:54 +0300

    Добавил отправку приколов

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 //Короч, тут просто будет отправлялка почты сч
+
+form.onsubmit = async (e) => {
+    e.preventDefault();
+    let data = document.getElementById("email").value
+    let response = await fetch('http://85.209.2.93/graphql/', {
+        method: 'POST',
+        body: `mutation{addBetatester(email: "${data}"){ok}}`
+    });
+
+    let result = await response.json();
+
+    alert("Your email successfully received");
+};
