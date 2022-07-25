32496c64a2860cbb6c27999e5b478c4621ff9769
sufimtsev <sufimtsev@ozon.ru>
2022-07-25 10:38:46 +0300

    обновил много чего

--- index.js
+++ index.js
@@ -2,13 +2,18 @@
 
 form.onsubmit = async (e) => {
     e.preventDefault();
+    submit.classList.toggle("loading")
     let data = document.getElementById("email").value
     let response = await fetch('http://85.209.2.93/graphql/', {
         method: 'POST',
         body: `mutation{addBetatester(email: "${data}"){ok}}`
+    }).then(resp => {
+        alert("Your email successfully received");
+        submit.classList.toggle("loading")
+    }).catch(error => {
+        console.error('There was an error!', error);
+        alert("Error please try later");
+        submit.classList.toggle("loading")
     });
 
-    let result = await response.json();
-
-    alert("Your email successfully received");
 };
