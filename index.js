a9185ddd64cfcd7ad9e9994d450c690fa4f9295e
sufimtsev <sufimtsev@ozon.ru>
2022-07-25 11:09:00 +0300

    Сделал штуку чтоб один и тот же имейл не писали

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 //Короч, тут просто будет отправлялка почты сч
 
+betatasets = []
+
 form.onsubmit = async (e) => {
     e.preventDefault();
     submit.classList.toggle("loading")
     let data = document.getElementById("email").value
+    for (tester in betatasets) {
+        if (tester === data) {
+            alert("This email is already registered")
+            return
+        }
+    }
     let response = await fetch('https://nemem.app/app/graphql/', {
         method: 'POST',
         headers: {"content-type": "application/json",},
@@ -11,6 +19,7 @@ form.onsubmit = async (e) => {
     }).then(resp => {
         alert("Your email successfully received");
         submit.classList.toggle("loading")
+        betatasets.append(data)
     }).catch(error => {
         console.error('There was an error!', error);
         alert("Error please try later");
